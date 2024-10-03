992083d890696c6dd9a514f5129209ed54cf1fd3
Osama Dawabsheh <omad10200099@gmail.com>
2024-10-03 14:31:46 +0300

    first commit

---
+++ Library.PL/Areas/Dashboard/Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.PL.Areas.Dashboard.Controllers
+{
+    public class BooksController : Controller
+    {
+        [Area("Dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
